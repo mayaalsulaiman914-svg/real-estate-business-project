97d8a2534842c59dcaa5d94a3579592e48fa34d0
rahma <alsalamarahma883@gmil.com>
2026-09-09 23:07:11 +0300

    Smartt

---
+++ src/components/Smart/SmartCards.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ServiceCard from "../ServiceCard/ServiceCard";
+import smartCardsData from "../../data/SmartCard";
+
+import "./SmartCards.css";
+
+const SmartCards = () => {
+  return (
+    <div className="smart-cards">
+      {smartCardsData.map((card) => (
+        <ServiceCard
+          key={card.id}
+          icon={card.icon}
+          title={card.title}
+          description={card.description}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SmartCards;
